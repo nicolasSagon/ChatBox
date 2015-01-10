20afe280bdabf08c9bb13d114d27bf7de28331b2
N3ness <pribierre@gmail.com>
2015-01-10 03:07:35 +0100

    Ajout du code générique serveur et client

--- ClientChatBox.c
+++ ClientChatBox.c
@@ -1,7 +1,57 @@
-#include "protocole.h";
-
-int main(int argc, char** argv){
-	
-	printf("Client lancé\n");
-	return 0;
+#include <protocole.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERVER_PORT 1500
+#define MAX_MSG 80
+int main (int argc, char *argv[])
+{
+  int sd, i;
+  struct sockaddr_in client_addr, serv_addr;
+  // Command-line error check
+  if (argc < 3)
+    return 1;
+  printf("%s: trying to send to %s\n", argv[0], argv[1]);
+  // Create socket
+  if ((sd = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
+  {
+    perror(argv[0]);
+    return 1;
+  }
+  // Bind socket
+  client_addr.sin_family = AF_INET;
+  client_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  client_addr.sin_port        = htons(0);
+  if (bind(sd,(struct sockaddr *)&client_addr, sizeof client_addr) == -1)
+  {
+    perror("bind");
+    return 1;
+  }
+  // Fill server address structure
+  serv_addr.sin_family = AF_INET;
+  if (inet_aton(argv[1], &(serv_addr.sin_addr)) == 0)
+  {
+    printf("Invalid IP address format <%s>\n", argv[1]);
+    return 1;
+  }
+  serv_addr.sin_port = htons(SERVER_PORT);
+  // send all messages
+  for (i = 2; i < argc; i++)
+  {
+    if (sendto(sd, argv[i], strlen(argv[i]) + 1, 0,
+          (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+      perror("sendto");
+      return 1;
+    } else {
+      printf("Sent to %s: %s\n", inet_ntoa(serv_addr.sin_addr), argv[i]);
+    }
+  }
+  close(sd);
+  return 0;
 }
+
+
--- ServeurChatBox.c
+++ ServeurChatBox.c
@@ -1,7 +1,45 @@
 #include "protocole.h";
-
-int main(int argc, char** argv){
-
-	printf("Serveur lancé\n");
-	return 0;
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define SERVER_PORT 1500
+#define MAX_MSG 80
+int main(void)
+{
+  int sd, n;
+  socklen_t addr_len;
+  struct sockaddr_in client_addr, server_addr;
+  char msgbuf[MAX_MSG];
+  // Create socket
+  if ((sd = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
+  {
+    perror("socket creation");
+    return 1;
+  }
+  // Bind it
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(SERVER_PORT);
+  if (bind(sd, (struct sockaddr *)&server_addr, sizeof server_addr) == -1)
+  {
+    perror("bind");
+    return 1;
+  }
+  for (;;)
+  {
+    addr_len = sizeof(client_addr);
+    n = recvfrom(sd, msgbuf, MAX_MSG, 0,
+        (struct sockaddr *)&client_addr, &addr_len);
+    if (n == -1)
+      perror("recvfrom");
+    else {
+      printf("received from %s: %s\n",
+          inet_ntoa(client_addr.sin_addr), msgbuf);
+    }
+  }
+  return 0;
 }
+
+
